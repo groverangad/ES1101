513de16a1b950873ba0a8b18bf50992e208e0374
groverangad <103065677+groverangad@users.noreply.github.com>
2022-05-06 18:57:27 +0530

    Update Question 5.py

--- Assignment_2/Question 5.py
+++ Assignment_2/Question 5.py
@@ -1,5 +1,9 @@
-#Question 5
-
+'''Question 5
+For any three lengths, there is a simple test to see if it is possible to form a
+triangle. If any of the three lengths is greater than the sum of the other two,
+then you cannot form a triangle. Otherwise, Enter three sides of a triangle,
+converts them to integers, and to check whether the given input lengths can
+form a triangle or not (Print : “Yes” or “No”).[Don’t use if else here].'''
 
 A1=first_number=int(input("enter side 1: "))
 A2=second_number=int(input("enter 2nd side: "))
