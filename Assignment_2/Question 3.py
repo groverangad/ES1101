61b2d811f1335b0d7c4aa50a911038620428b79a
groverangad <103065677+groverangad@users.noreply.github.com>
2022-05-06 18:56:10 +0530

    Update Question 3.py

--- Assignment_2/Question 3.py
+++ Assignment_2/Question 3.py
@@ -1,4 +1,10 @@
-#Question 3
+'''Question 3
+For a=56 and b=10 with the help of bitwise operators calculate the following:
+a. a&b
+b. a|b
+c. a^b
+d. Left shift both a and b with 2 bits.
+e. Right shift a with 2 bits and b with 4 bits.'''
 
 
 a=56
