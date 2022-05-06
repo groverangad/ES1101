b97d315896ecf96eee0637b7eb6a47f219cb9282
groverangad <103065677+groverangad@users.noreply.github.com>
2022-05-06 18:55:08 +0530

    Update Question 1.py

--- Assignment_2/Question 1.py
+++ Assignment_2/Question 1.py
@@ -1,4 +1,12 @@
-#Question1 
+'''Question1 
+For a given input string “Python is a case sensitive language”. Write python
+code for the following:
+a. Find the length of the input string.
+b. Reverse the order of the string in one line code.
+c. Using Slice function store “a case sensitive” in new string.
+d. Replace “a case sensitive” with “object oriented”.
+e. Find index of substring “a” in the given input string.
+f. Remove the white spaces from the given input string.'''
 
 
 Given_string="python is a case sensitive language"
