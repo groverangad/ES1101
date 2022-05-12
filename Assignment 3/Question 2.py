6c4ecf97686bee2acdd8f0a01f99c1b2412c957c
groverangad <103065677+groverangad@users.noreply.github.com>
2022-05-12 21:11:17 +0530

    Update Question 2.py

--- Assignment 3/Question 2.py
+++ Assignment 3/Question 2.py
@@ -1,28 +1,6 @@
-print('''Welcome to The calculator. Please choose the operation
-      
-     1 - Addition
-     2 - Subtraction
-     3 - Multiplication
-     4 - Division ''')
 
-operation = int(input("operation operations form 1, 2, 3, 4 :"))
-  
-number_1 = int(input("Enter first number: "))
-number_2 = int(input("Enter second number: "))
-  
-if operation == 1:
-    print(number_1, "+", number_2, "=",number_1+number_2)
-                   
-  
-elif operation == 2:
-    print(number_1, "-", number_2, "=",number_1-number_2)
-                   
-  
-elif operation == 3:
-    print(number_1, "*", number_2, "=",number_1*number_2)
-                    
-  
-elif operation == 4:
-    print(number_1, "/", number_2, "=", number_1/number_2)
-else:
-    print("Invalid input")
+print("For Interactive Python Calculator")
+enter_your_expression = input("Enter the expressions by using only these operators (+,-,*,/,**,%,//):")
+print("The result is ", end=" ")
+print(eval(enter_your_expression))
+print("\n")
