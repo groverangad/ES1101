2f234ae3297f23e869600feff966b9aaf2739e39
groverangad <103065677+groverangad@users.noreply.github.com>
2022-06-14 15:24:06 +0530

    Update Question 1.py
    added comments for reference

--- Assignment_1/Question 1.py
+++ Assignment_1/Question 1.py
@@ -3,13 +3,15 @@ Question_1="Write a Python program to find average of three numbers"\
     " entered by the user."
     
 print(Question_1,"\n")
- 
+
+#Taking 3 inputs from user 
 N1=int(input("Enter the first number = "))
 
 N2=int(input("Enter the second number = "))
 
 N3=int(input("Enter the third number = "))
 
+#Calculating average
 Avg =((N1+N2+N3)/3)
 
 print("The average of the three entered numbers is ", Avg)
